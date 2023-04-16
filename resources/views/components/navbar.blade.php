21306d83d5671d66c6439eff1b129d95b363b331
DarioMedugno <>
2023-04-16 23:04:24 +0200

    progetto completato

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg bg-light p-0 position-fixed w-100">
+    <div class="container-fluid bg-black costum-border ">
+        <a class="navbar-brand text-light" href="#"> DAGG Project</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active text-light" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="#">Serie TV</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="#">Chi siamo</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
